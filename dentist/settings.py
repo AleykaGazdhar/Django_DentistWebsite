842e87829c4abc04229d9ad2b2ff6f510cac39a8
Aleyka Gazdhar <gazdharaleyka01@gmail.com>
2022-03-13 16:58:29 +0530

    Second Commit

--- dentist/settings.py
+++ dentist/settings.py
@@ -126,9 +126,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER =  config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER =  config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False 
 
 django_heroku.settings(locals())
+
